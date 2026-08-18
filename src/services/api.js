af2b54294bc11ee84a492edbb3ee3f11b966c128
Gourav Chaturvedi <gauravchaturvedi727@gmail.com>
2026-08-18 14:49:29 +0530

    Update API base URL to include '/api' path

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://sports-partner-finder-backend-1.onrender.com"
+    baseURL: "https://sports-partner-finder-backend-1.onrender.com/api"
 });
 
 api.interceptors.request.use((config) =>{
